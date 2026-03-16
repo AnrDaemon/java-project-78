ca6301bceddcc11737515f4272122845490380d7
Andrey Repin <anrdaemon@yandex.ru>
2026-03-16 07:27:03 +0300

    Added NumberSchema
    Improved StringSchema tests.

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -1,11 +1,15 @@
 package hexlet.code;
 
+import hexlet.code.schemas.NumberSchema;
 import hexlet.code.schemas.StringSchema;
 
 public final class Validator {
 
+    public NumberSchema number() {
+        return new NumberSchema();
+    }
+
     public StringSchema string() {
         return new StringSchema();
     }
-
 }
