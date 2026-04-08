64fa25b8e8417a56831324445d4787885b423198
AnrDaemon <anrdaemon@yandex.ru>
2026-04-08 23:36:29 +0300

    Post 4: Attempt to cover system.exit (#9)
    * Properly placed schema tests
    
    * Added system.exit test

--- app/src/test/java/hexlet/code/schemas/MapSchemaTest.java
+++ app/src/test/java/hexlet/code/schemas/MapSchemaTest.java
@@ -1,4 +1,4 @@
-package hexlet.code;
+package hexlet.code.schemas;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -12,8 +12,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import hexlet.code.schemas.BaseSchema;
-import hexlet.code.schemas.MapSchema;
+import hexlet.code.Validator;
 
 public class MapSchemaTest {
 
--- app/src/test/java/hexlet/code/schemas/NumberSchemaTest.java
+++ app/src/test/java/hexlet/code/schemas/NumberSchemaTest.java
@@ -1,4 +1,4 @@
-package hexlet.code;
+package hexlet.code.schemas;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -10,7 +10,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import hexlet.code.schemas.NumberSchema;
+import hexlet.code.Validator;
 
 public class NumberSchemaTest {
 
